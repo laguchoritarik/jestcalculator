c330a1e25463eae144116ce15055abea0d0c3dd5
tariklaguchori <tarik.lagu@gmail.com>
2025-04-15 01:22:58 +0100

    testuiiing

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,9 +1,13 @@
-const { add, subtract } = require('../src/calculator');
+const { add, subtract, multiply } = require('../src/calculator');
 
 test('adds numbers', () => {
-  expect(add(5, 3)).toBe(9);
+  expect(add(5, 3)).toBe(8);
 });
 
 test('subtracts numbers', () => {
   expect(subtract(10, 4)).toBe(6);
 });
+test("multiplication de deux reels ",()=>
+{
+  expect(multiply(9,8)).toBe(72);
+})
